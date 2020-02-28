d8781ccff5ce8fa3c37646c7839235457ef56a35
Adam Paulukanis <adam.paulukanis@gmail.com>
2020-02-28 16:23:35 +0000

    Add padZero and a few small changes
    - padZero(): number smaller than 10? Add padding
    - the due date: my arrival at my block of flats
    - if the due date is 100 days in future just show 99+ days

--- function.js
+++ function.js
@@ -1,6 +1,14 @@
 (function (root) {
   const year = new Date().getFullYear()
-  const chooseDate = new Date('2020-03-10T08:55').getTime()
+  const chooseDate = new Date('2020-03-10T20:00').getTime()
+
+  function padZero (number) {
+    let numberString = String(number);
+    while (numberString.length < 2) {
+      numberString = '0' + numberString;
+    }
+    return numberString;
+  }
 
   let countdown = setInterval(function () {
     const today = new Date().getTime()
@@ -11,11 +19,23 @@
     let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
     let seconds = Math.floor((diff % (1000 * 60)) / 1000)
 
-    document.getElementById('countdown').innerHTML =
-      '<div class="days"><div class="c-number">' + days + '</div>dni</div>' +
-      '<div class="hours"><div class="c-number">' + hours + '</div>godziny</div>' +
-      '<div class="minutes"><div class="c-number">' + minutes + '</div>minuty</div>' +
-      '<div class="seconds"><div class="c-number">' + seconds + '</div>sekundy</div>' +
-      '<div class="left">ZOSTAŁO</div></div>'
+    if (days > 99) {
+      days = '99+';
+      document.getElementById('countdown').innerHTML =
+        '<div class="days"><div class="c-number">' + days + '</div>dni</div>' +
+        '<div class="left">ZOSTAŁO</div></div>'
+    } else {
+      days = padZero(days);
+      hours = padZero(hours);
+      minutes = padZero(minutes);
+      seconds = padZero(seconds);
+
+      document.getElementById('countdown').innerHTML =
+        '<div class="days"><div class="c-number">' + days + '</div>dni</div>' +
+        '<div class="hours"><div class="c-number">' + hours + '</div>godziny</div>' +
+        '<div class="minutes"><div class="c-number">' + minutes + '</div>minuty</div>' +
+        '<div class="seconds"><div class="c-number">' + seconds + '</div>sekundy</div>' +
+        '<div class="left">ZOSTAŁO</div></div>'
+    }
   }, 1000)
 }(this))
