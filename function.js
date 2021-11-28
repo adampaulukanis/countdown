ec42e30944b10399810d4fb73d82f88e219a48f2
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-11-28 19:42:01 +0100

    A small change

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
 (function (root) {
-  const year = new Date().getFullYear()
-  const chooseDate = new Date('2020-03-09T06:00').getTime()
+  const year = new Date().getFullYear();
+  const now = returnDate(location.search) || '2022-01-01T19:22';
+  const chooseDate = new Date(now).getTime();
 
-  function padZero (number) {
+  function padZero(number) {
     let numberString = String(number);
     while (numberString.length < 2) {
       numberString = '0' + numberString;
@@ -11,19 +12,21 @@
   }
 
   let countdown = setInterval(function () {
-    const today = new Date().getTime()
-    const diff = chooseDate - today
+    const today = new Date().getTime();
+    const diff = chooseDate - today;
 
-    let days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
-    let seconds = Math.floor((diff % (1000 * 60)) / 1000)
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
     if (days > 99) {
       days = '99+';
       document.getElementById('countdown').innerHTML =
-        '<div class="days"><div class="c-number">' + days + '</div>dni</div>' +
-        '<div class="left">ZOSTAŁO</div></div>'
+        '<div class="days"><div class="c-number">' +
+        days +
+        '</div>dni</div>' +
+        '<div class="left">ZOSTAŁO</div></div>';
     } else {
       days = padZero(days);
       hours = padZero(hours);
@@ -31,11 +34,24 @@
       seconds = padZero(seconds);
 
       document.getElementById('countdown').innerHTML =
-        '<div class="days"><div class="c-number">' + days + '</div>dni</div>' +
-        '<div class="hours"><div class="c-number">' + hours + '</div>godziny</div>' +
-        '<div class="minutes"><div class="c-number">' + minutes + '</div>minuty</div>' +
-        '<div class="seconds"><div class="c-number">' + seconds + '</div>sekundy</div>' +
-        '<div class="left">ZOSTAŁO</div></div>'
+        '<div class="days"><div class="c-number">' +
+        days +
+        '</div>dni</div>' +
+        '<div class="hours"><div class="c-number">' +
+        hours +
+        '</div>godziny</div>' +
+        '<div class="minutes"><div class="c-number">' +
+        minutes +
+        '</div>minuty</div>' +
+        '<div class="seconds"><div class="c-number">' +
+        seconds +
+        '</div>sekundy</div>' +
+        '<div class="left">ZOSTAŁO</div></div>';
     }
-  }, 1000)
-}(this))
+  }, 1000);
+})(this);
+
+function returnDate(search) {
+  let dateParam = new URLSearchParams(search);
+  return dateParam.get('date');
+}
